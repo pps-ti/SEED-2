57ec6903e8f64601bd95bef5036c8efb9101edb9
Yunus Oktavianto Ismail <yunusismail99@gmail.com>
2020-10-12 22:17:26 +0700

    SEED-2

--- README.md
+++ README.md
@@ -3,4 +3,31 @@
 </p>
 
 # SEED-2
-Materi Software Engineering EDucation pertemuan kedua
+Pada Pertemuan kedua ini Software Engineering EDucation membahas mengenai <i>cloud storage/database</i> menggunakan <b>MongoDB</b>
+
+## :memo: Panduan memulai
+Silahkan ketik perintah berikut ini secara berurutan pada terminal(Linux/Mac) atau CMD(Windows) kalian jika ingin mencoba untuk menjalankannya pada <i>localhost</i> masing-masing :
+
+```
+git clone https://github.com/pps-ti/SEED-2
+cd SEED-2
+```
+atau
+```
+git init
+git pull https://github.com/pps-ti/SEED-2
+```
+
+<b>Jalankan di localhost</b>
+```
+python3 main.py
+```
+atau
+```
+python main.py
+```
+
+<b>Tampilan Halaman</b><br><br>
+![permalink setting demo](https://ibb.co/kSTf4bm)
+Ketik <b>ctrl + c</b> pada terminal/CMD kalian jika ingin mematikan server flask'nya.<br><br>
+<b><u>PENTING!!!</u></b> : Karena memperhatikan faktor keamanan pada <i>cloud storage/database</i>, kami tidak mencantumkan <i>API keys</i> dari Firebase dan MongoDB serta akun yang digunakan untuk menggunakan flask mail. Jadi merupakan hal yang wajar apabila jika menjalankan program ini secara <i>default</i> setelah kalian melakukan <i>pull</i>/<i>clone</i> repositori ini akan terjadi kegagalan.
--- main.py
+++ main.py
@@ -2,35 +2,36 @@ from pymongo import MongoClient
 from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__)
-koneksi = MongoClient("")
+client = MongoClient("")
+db = client.worshop_mongodb
+collection = db.crud
 
-db = koneksi.workshop_mongodb
-col_sifo = db.crud
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    data = [i for i in col_sifo.find()]
+    data = [i for i in collection.find()]
     if request.method == "POST":
-        col_sifo.insert_one({'_id': request.form['Npm'],
-                            'Nama': request.form['Nama'],
-                            'Jurusan': request.form['Jurusan']})
+        collection.insert_one({'_id': request.form['NPM'],
+                               'Nama': request.form['Nama'],
+                               'Jurusan': request.form['Jurusan']})
         return redirect(url_for('index'))
     return render_template('index.html', data=data)
 
-@app.route('/edit/<npm>', methods=['GET','POST'])
+
+@app.route('/edit/<npm>', methods=['GET', 'POST'])
 def edit(npm):
-    data = col_sifo.find_one({'_id': npm})
+    data = collection.find_one({'_id': npm})
     if request.method == 'POST':
-        col_sifo.update_one({'_id': request.form['Npm']},
-                            {"$set": {"Nama": request.form['Nama'],
-                            "Jurusan": request.form['Jurusan']}
-                            })
+        collection.update_one({'_id': request.form['NPM']},
+                              {'$set': {'Nama': request.form['Nama'],
+                                        'Jurusan': request.form['Jurusan']}})
         return redirect(url_for('index'))
     return render_template('edit.html', data=data)
 
+
 @app.route('/hapus/<npm>')
 def hapus(npm):
-    col_sifo.delete_one({'_id':npm})
+    collection.delete_one({'_id': npm})
     return redirect(url_for('index'))
 
-#app.run(debug=True)
+# app.run(debug=True)
